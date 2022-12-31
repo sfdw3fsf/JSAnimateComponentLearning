b238aa838df2e0f7cd145de62efc4298fad64f7b
VawnHuy <huy820op@gmail.com>
2022-12-31 23:07:08 +0700

    scroll

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
+  btnCloseModal.classList.add('is-active');
+  btnCloseModal.classList.remove('is-hidden');
+  overlay.classList.add('is-active');
+  overlay.classList.remove('is-hidden');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -17,9 +22,9 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +34,50 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+//////////////////////
+const header = document.querySelector('.header')
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = 'We use cookies to improve performance <button class="btn btn---close--cookie">Got It!</button>'
+// header.prepend(message.cloneNode(true));
+header.append(message);
+//Delete
+document.querySelector('.btn---close--cookie').addEventListener('click', function () {
+  // message.remove();
+  message.parentElement.removeChild(message);
+})
+//STYLE
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+console.log(getComputedStyle(message).height);
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px'
+
+// message.style.height = '80px'
+
+// document.documentElement.style.setProperty('--color-primary', 'orange')
+
+// const logo = document.querySelector('.nav__logo')
+// console.log(logo.src);
+// console.log(logo.getAttribute('src'));
+
+// const link = document.querySelector('.nav__link');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const sec1 = document.querySelector('#section--1');
+//Scrolling
+btnScrollTo.addEventListener('click', function () {
+  //OLD WAY
+  // const s1coords = sec1.getBoundingClientRect()
+  // console.log(s1coords);
+  // window.scrollTo({
+  //   // s1coords.left + window.pageXOffset, 
+  //   // s1coords.top + window.pageYOffset);
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth'
+  // });
+
+  //MODERN WAY
+  sec1.scrollIntoView({ behavior: 'smooth' })
+})
